1e7b60aecf02f7285969f6a41f12355e65b2b321
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-10-22 15:00:15 +0300

    added code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import LinearProgress from '@mui/material/LinearProgress';
+import Box from '@mui/material/Box';
 import * as authOperations from './redux/auth/auth-operations';
 import ApplicationBar from './components/ApplicationBar';
 import Container from './App.styled';
-import { RegisterView, LoginView, ContactsView, PageErrorView } from './views';
 import PrivatRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import { getLoadingStatus } from './redux/auth/auth-selectors';
+
+const AsyncLoginView = lazy(() =>
+  import('./views/LoginView' /* webpackChunkName: "login-page" */),
+);
+const AsyncRegisterView = lazy(() =>
+  import('./views/RegisterView' /* webpackChunkName: "register-page" */),
+);
+const AsyncContactsView = lazy(() =>
+  import('./views/ContactsView' /* webpackChunkName: "contacts-page" */),
+);
+const AsyncPageErrorView = lazy(() =>
+  import('./views/PageErrorView' /* webpackChunkName: "404-page" */),
+);
 
 const App = () => {
   const isUserDataLoading = useSelector(getLoadingStatus);
@@ -23,21 +37,29 @@ const App = () => {
       <ApplicationBar />
       <ToastContainer />
       <Container>
-        <Switch>
-          <PublicRoute path="/" exact restricted>
-            <Redirect to="/login" />
-          </PublicRoute>
-          <PublicRoute path="/login" restricted>
-            <LoginView />
-          </PublicRoute>
-          <PublicRoute path="/register" restricted>
-            <RegisterView />
-          </PublicRoute>
-          <PrivatRoute path="/contacts">
-            {!isUserDataLoading && <ContactsView />}
-          </PrivatRoute>
-          <Route component={PageErrorView} />
-        </Switch>
+        <Suspense
+          fallback={
+            <Box sx={{ width: '100%' }}>
+              <LinearProgress />
+            </Box>
+          }
+        >
+          <Switch>
+            <PublicRoute path="/" exact restricted>
+              <Redirect to="/login" />
+            </PublicRoute>
+            <PublicRoute path="/login" restricted>
+              <AsyncLoginView />
+            </PublicRoute>
+            <PublicRoute path="/register" restricted>
+              <AsyncRegisterView />
+            </PublicRoute>
+            <PrivatRoute path="/contacts">
+              {!isUserDataLoading && <AsyncContactsView />}
+            </PrivatRoute>
+            <Route component={AsyncPageErrorView} />
+          </Switch>
+        </Suspense>
       </Container>
     </>
   );
--- src/views/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ContactsView } from './ContactsView';
-export { default as RegisterView } from './RegisterView';
-export { default as LoginView } from './LoginView';
-export { default as PageErrorView } from './PageErrorView';
