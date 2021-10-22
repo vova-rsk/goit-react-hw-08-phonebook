6cf20f056be0ebe6cc6fc0acee81803fc0bb289d
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-10-22 17:44:35 +0300

    added loader

--- src/components/Contacts/ContactsList/ContactsList.jsx
+++ src/components/Contacts/ContactsList/ContactsList.jsx
@@ -2,12 +2,18 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as contactsOperations from '../../../redux/contacts/contacts-operations';
 import ContactItem from '../ContactsItem';
-import { getFilteredContacts } from '../../../redux/contacts/contacts-selectors';
+import {
+  getFilteredContacts,
+  getLoadingStatus,
+} from '../../../redux/contacts/contacts-selectors';
 import StyledList from './ContactsList.styled';
 import { resetContacts } from '../../../redux/contacts/contacts-slice';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 const ContactList = () => {
   const filteredContacts = useSelector(getFilteredContacts);
+  const isLoading = useSelector(getLoadingStatus);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,11 +22,26 @@ const ContactList = () => {
   }, [dispatch]);
 
   return (
-    <StyledList>
-      {filteredContacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
-    </StyledList>
+    <>
+      {isLoading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <StyledList>
+          {filteredContacts.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
+        </StyledList>
+      )}
+    </>
   );
 };
 
